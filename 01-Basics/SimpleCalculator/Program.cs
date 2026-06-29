da82eca2413b722b9a8e398597523f678dbd98f2
Ayat6102Mohamed <166727300+Ayat6102Mohamed@users.noreply.github.com>
2026-06-29 12:27:55 +0300

    Add project structure with folders

---
+++ 01-Basics/SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 3;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Addition: {Add(a, b)}");
+            Console.WriteLine($"Subtraction: {Subtract(a, b)}");
+            Console.WriteLine($"Multiplication: {Multiply(a, b)}");
+            int rem;
+            Console.WriteLine($"Division: {Divide(a, b, out rem)}, Remainder: {rem}");
+
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static int Divide(int a, int b, out int rem)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            rem = a % b;
+            return a / b;
+        }
+
+    }
+}
